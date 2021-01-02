0dc4146adce8d904e58b75e4f501344972aa0fc3
jorgevargasjvm <45214319+jorgevargasjvm@users.noreply.github.com>
2021-01-03 00:22:46 +0100

    Página de inicio parte dos

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -4,3 +4,4 @@ export const STATISTICS = ADMIN+"/statistics";
 export const USERS = ADMIN +"/users";
 export const NOTIFICATIONS = ADMIN +"/notifications";
 export const EVENTS = ADMIN +"/events";
+export const PURCHASE = "/purchase";
